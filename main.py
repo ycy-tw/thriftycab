f8589360619aa1e6fbefb196061e427358cc62e3
jiapei-wu <69879618+jiapei-wu@users.noreply.github.com>
2020-12-21 09:42:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,10 +50,9 @@ def arrive_click_searching():
     Location_suggestions_dropdown_list = OptionMenu(window, default_variable, *Location_suggestions)
     Location_suggestions_dropdown_list.place(x=423, y= 369)
 
-
+# 視窗頁面
 window = tk.Tk()
 window.title('Thrifty Cab')
-# window = iconbitmap
 window.geometry('800x600')
 window.configure(bg='floral white')
 window.resizable(height='False', width='False')
@@ -71,9 +70,7 @@ start_entry.config(highlightbackground="bisque")
 search_button = Button(text='search', width=3, relief=GROOVE, command=start_click_searching)
 search_button.place(x=360, y=305)
 search_button.config(highlightbackground="bisque")
-# clear_button = Button(text='clear', width=3, relief=GROOVE) #command=
-# clear_button.place(x=423, y=305)
-# clear_button.config(highlightbackground="bisque")
+
 
 frm_arrive = tk.Frame(width=760, height=40, background="bisque")
 frm_arrive.place(x=20, y=360)
@@ -85,22 +82,7 @@ arrive_entry.config(highlightbackground="bisque")
 search_button = Button(text='search', width=3, relief=RAISED, command=arrive_click_searching)
 search_button.place(x=360, y=365)
 search_button.config(highlightbackground="bisque")
-# clear_button = Button(text='clear', width=3, relief=GROOVE) #command=
-# clear_button.place(x=423, y=365)
-# clear_button.config(highlightbackground="bisque")
 
-# ent1 = tk.Entry(width=30)
-# ent1.place(x=300, y=50)
-# # start = entry.get()
-# # entry.delete(0, tk.END)
-
-# frm_2 = tk.Frame()
-# frm_2.place(x=200, y=80)
-# lbl_2 = tk.Label(master=frm_2, text = 'Where to?', font = ('Telugu MN', 16))
-# lbl_2.pack()
-
-# ent2 = tk.Entry(width=30)
-# ent2.place(x=300, y=80)
 
 
 
