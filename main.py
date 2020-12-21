f0abe2dedaf3a78df4765fad54bdfa5115a120f3
jiapei-wu <69879618+jiapei-wu@users.noreply.github.com>
2020-12-21 12:03:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,28 +27,38 @@ def estimate_all(start_loc, stop_loc):
 def start_click_searching():
 
     global start_entry
+    global start_location_suggestions_dropdown_list
 
     keywords = start_entry.get()
     crawler = Crawler()
-    Location_suggestions = crawler.location_suggestions(keywords)
-
+    start_location_suggestions = crawler.location_suggestions(keywords)
+    try:
+        start_location_suggestions_dropdown_list.destroy()
+    except:
+        pass
     default_variable = StringVar(window)
-    default_variable.set(Location_suggestions[0]) # default value
-    Location_suggestions_dropdown_list = OptionMenu(window, default_variable, *Location_suggestions)
-    Location_suggestions_dropdown_list.place(x=423, y= 309)
+    default_variable.set(start_location_suggestions[0]) # default value
+    start_location_suggestions_dropdown_list = OptionMenu(window, default_variable, *start_location_suggestions)
+    start_location_suggestions_dropdown_list.place(x=423, y= 309)
 
 def arrive_click_searching():
 
     global arrive_entry
+    global arrive_location_suggestions_dropdown_list
 
     keywords = arrive_entry.get()
     crawler = Crawler()
-    Location_suggestions = crawler.location_suggestions(keywords)
-
+    arrive_location_suggestions = crawler.location_suggestions(keywords)
+    try:
+        arrive_location_suggestions_dropdown_list.destroy()
+    except:
+        pass
     default_variable = StringVar(window)
-    default_variable.set(Location_suggestions[0]) # default value
-    Location_suggestions_dropdown_list = OptionMenu(window, default_variable, *Location_suggestions)
-    Location_suggestions_dropdown_list.place(x=423, y= 369)
+    default_variable.set(arrive_location_suggestions[0]) # default value
+    arrive_location_suggestions_dropdown_list = OptionMenu(window, default_variable, *arrive_location_suggestions)
+    arrive_location_suggestions_dropdown_list.place(x=423, y= 369)
+
+def thrifty_searching():
 
 # 視窗頁面
 window = tk.Tk()
@@ -81,12 +91,13 @@ lbl_arrive.place(x=50, y=362)
 arrive_entry = tk.Entry(bg="gray88", relief=GROOVE, width=18,)#,textvariable=v
 arrive_entry.place(x=185, y=366)
 arrive_entry.config(highlightbackground="bisque")
-search_button = Button(text='search', width=3, relief=RAISED, command=arrive_click_searching)
+search_button = Button(text='search', width=3, relief=GROOVE, command=arrive_click_searching)
 search_button.place(x=360, y=365)
 search_button.config(highlightbackground="bisque")
 
-
-
-
+# 'Thrifty' button
+thrifty_btn = Button(width=10, height=1, text='Thrifty!', relief=GROOVE, font = ("Telugu MN", 20), bg='snow')
+thrifty_btn.place(x=330, y=420)
+thrifty_btn.config(highlightbackground='floral white')
 
 window.mainloop()
