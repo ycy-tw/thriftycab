504c90c9890a440af3118aff6e7cfff2e270adf1
jiapei-wu <69879618+jiapei-wu@users.noreply.github.com>
2020-12-27 11:45:41 +0800

    Update test_mainpage.py

--- test_mainpage.py
+++ test_mainpage.py
@@ -7,7 +7,7 @@ class MainPage():
     def __init__(self, master=None, mail=None, pwd=None):
 
         self.root = master #定義內部變數root
-        self.root.geometry('1000x750') #設定視窗大小
+        self.root.geometry('800x600') #設定視窗大小
         self.root.configure(bg='floral white')
         self.root.resizable(height='False', width='False')
 
@@ -32,14 +32,14 @@ class MainPage():
 
         self.start_default_variable = StringVar(self.root)
         self.start_default_variable.set(start_location_suggestions[0]) # default value
-        start_location_suggestions_dropdown_list = OptionMenu(self.root, self.start_default_variable, *start_location_suggestions)
-        start_location_suggestions_dropdown_list.place(x=423, y= 309)
+        self.start_location_suggestions_dropdown_list = OptionMenu(self.root, self.start_default_variable, *start_location_suggestions)
+        self.start_location_suggestions_dropdown_list.place(x=423, y= 129)
 
 
     def arrive_click_searching(self):
 
         # global arrive_entry
-        # global arrive_location_suggestions_dropdown_list
+        global arrive_location_suggestions_dropdown_list
         # global arrive_default_variable
 
         keywords = self.arrive_entry.get()
@@ -53,8 +53,8 @@ class MainPage():
 
         self.arrive_default_variable = StringVar(self.root)
         self.arrive_default_variable.set(arrive_location_suggestions[0]) # default value
-        arrive_location_suggestions_dropdown_list = OptionMenu(self.root, self.arrive_default_variable, *arrive_location_suggestions)
-        arrive_location_suggestions_dropdown_list.place(x=423, y= 369)
+        self.arrive_location_suggestions_dropdown_list = OptionMenu(self.root, self.arrive_default_variable, *arrive_location_suggestions)
+        self.arrive_location_suggestions_dropdown_list.place(x=423, y= 189)
 
 
     def thrifty_searching(self, start_loc=None, stop_loc=None):
@@ -84,15 +84,15 @@ class MainPage():
 
         city_price = estimate.m_taxi(coordinates)
         print('city成功', city_price)
+        # len(mail) != 0:
+        if mail is None:
 
-        if len(mail) != 0:
-
-            line_taxi_price = estimate.line_taxi(start_loc, stop_loc, mail, pwd)
-            print('line成功', line_taxi_price)
-
-        else:
             line_taxi_price = 0
             print('使用者不用line', line_taxi_price)
+
+        else:
+            line_taxi_price = estimate.line_taxi(start_loc, stop_loc, mail, pwd)
+            print('line成功', line_taxi_price)
 
         firm_list = ['Uber', 'LINE', 'TW', 'Metro']
         price_list = [uber_taxi_price, line_taxi_price, tw_taxi_price, city_price]
@@ -111,49 +111,49 @@ class MainPage():
             fare = result[i][1]
 
             if fare == 0:
-                fare = '無估計車資'
+                result[i][1] = '無估計車資'
 
             # 由小到大呈現在畫面上
             self.firm_name = Label(text=firm, font = ("Telugu MN", 20), bg="bisque")
-            self.firm_name.place(x= 100, y= 400  + i*20)
+            self.firm_name.place(x= 100, y= 400  + i*40)
 
             self.estimate_price = Label(text=result[i][1], font = ("Telugu MN", 20), bg="bisque")
-            self.estimate_price.place(x= 120, y= 400  + i*20)
+            self.estimate_price.place(x= 180, y= 400  + i*40)
             
 
     def CreatePage(self):
 
         # 標題
-        self.lbl_title = Label(text = 'Thrifty Cab', font=("Telugu MN", 100, 'bold italic'), bg='floral white')
-        self.lbl_title.place(x=110, y=150)
+        self.lbl_title = Label(text = 'Thrifty Cab', font=("Telugu MN", 60, 'bold italic'), bg='floral white')
+        self.lbl_title.place(x=220, y=30)
 
         # 起始點
         self.frm_start = Frame(width=760, height=40, background="bisque")
-        self.frm_start.place(x=20, y=300)
+        self.frm_start.place(x=20, y=120)
         self.lbl_start = Label(text = 'Pickup Location', font = ("Telugu MN", 20), bg="bisque")
-        self.lbl_start.place(x=28, y=302)
+        self.lbl_start.place(x=28, y=122)
         self.start_entry = Entry(bg="gray88", relief=GROOVE, width=18,)#,textvariable=v
-        self.start_entry.place(x=185, y=306)
+        self.start_entry.place(x=185, y=126)
         self.start_entry.config(highlightbackground="bisque")
         self.search_button = Button(text='search', width=3, relief=GROOVE, command=self.start_click_searching)
-        self.search_button.place(x=360, y=305)
+        self.search_button.place(x=360, y=125)
         self.search_button.config(highlightbackground="bisque")
 
         # 終點
         self.frm_arrive = Frame(width=760, height=40, background="bisque")
-        self.frm_arrive.place(x=20, y=360)
+        self.frm_arrive.place(x=20, y=180)
         self.lbl_arrive = Label(text = 'Where to?', font = ("Telugu MN", 20), bg="bisque")
-        self.lbl_arrive.place(x=50, y=362)
+        self.lbl_arrive.place(x=50, y=182)
         self.arrive_entry = Entry(bg="gray88", relief=GROOVE, width=18,)#,textvariable=v
-        self.arrive_entry.place(x=185, y=366)
+        self.arrive_entry.place(x=185, y=186)
         self.arrive_entry.config(highlightbackground="bisque")
         self.search_button = Button(text='search', width=3, relief=GROOVE, command=self.arrive_click_searching)
-        self.search_button.place(x=360, y=365)
+        self.search_button.place(x=360, y=185)
         self.search_button.config(highlightbackground="bisque")
 
         # 'Thrifty' button
         self.thrifty_btn = Button(width=10, height=1, text='Thrifty!', relief=GROOVE, font = ("Telugu MN", 20), bg='snow', command=self.thrifty_searching)
-        self.thrifty_btn.place(x=330, y=420)
+        self.thrifty_btn.place(x=330, y=240)
         self.thrifty_btn.config(highlightbackground='floral white')
 
 
