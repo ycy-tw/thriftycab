9566ffbb03931df28f7ed7754c038e9f59d9a271
jiapei-wu <69879618+jiapei-wu@users.noreply.github.com>
2021-01-02 22:56:05 +0800

    Update test_mainpage.py

--- test_mainpage.py
+++ test_mainpage.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import ttk
 from crawler import Crawler
 from PIL import ImageTk,Image
+import time
 #from view import * #選單欄對應的各個子頁面
 
 class MainPage():
@@ -40,7 +42,7 @@ class MainPage():
         self.start_default_variable.set(start_location_suggestions[0]) # default value
         self.start_location_suggestions_dropdown_list = OptionMenu(self.root, self.start_default_variable, *start_location_suggestions)
         self.start_location_suggestions_dropdown_list.config(width=34)
-        self.start_location_suggestions_dropdown_list.place(x=97, y= 265)
+        self.start_location_suggestions_dropdown_list.place(x=97, y= 275)
 
 
     def arrive_click_searching(self):
@@ -62,7 +64,7 @@ class MainPage():
         self.arrive_default_variable.set(arrive_location_suggestions[0]) # default value
         self.arrive_location_suggestions_dropdown_list = OptionMenu(self.root, self.arrive_default_variable, *arrive_location_suggestions)
         self.arrive_location_suggestions_dropdown_list.config(width=34)
-        self.arrive_location_suggestions_dropdown_list.place(x=97, y= 365)
+        self.arrive_location_suggestions_dropdown_list.place(x=97, y= 375)
 
 
     def thrifty_searching(self, start_loc=None, stop_loc=None):
@@ -86,7 +88,8 @@ class MainPage():
         uber_address_name = uber_result[4]
 
         print('uebr成功', uber_taxi_price)
-
+        self.progress = ttk.Progressbar(self.root, orient=HORIZONTAL, length=380, mode='determinate')
+        self.progress.place(x=200, y=190, pady=20)
         # 如果使用者沒輸入email的話，代表不使用line taxi車資估算
         if mail is None:
 
@@ -116,7 +119,7 @@ class MainPage():
 
         # 建立一塊區域呈現結果
         self.result_frame = Frame(self.root, bg='white')
-        self.result_frame.place(x=490, y=258)
+        self.result_frame.place(x=495, y=268)
 
         # 把沒結果(結果為0)的估計車資改成無估計車資
         for i in range(len(result)):
@@ -155,52 +158,52 @@ class MainPage():
 
         # 起始點
         self.frm_start = Frame(width=403, height=40, background="lemon chiffon")
-        self.frm_start.place(x=50, y=210)
+        self.frm_start.place(x=50, y=220)
         self.frm_startbox = Frame(width=30, height=30, background="lemon chiffon")
-        self.frm_startbox.place(x=55, y=263)
+        self.frm_startbox.place(x=55, y=273)
         self.frm_startlist = Frame(width=363, height=40, background="gray98", relief='solid', borderwidth=1)
-        self.frm_startlist.place(x=90, y=260)
+        self.frm_startlist.place(x=90, y=270)
         self.lbl_start = Label(text = 'Pickup Location', font = ("Telugu MN", 20), bg="lemon chiffon")
-        self.lbl_start.place(x=58, y=212)
+        self.lbl_start.place(x=58, y=222)
         self.start_entry = Entry(bg="gray88", relief=GROOVE, width=18,)#,textvariable=v
-        self.start_entry.place(x=215, y=216)
+        self.start_entry.place(x=215, y=226)
         self.start_entry.config(highlightbackground="lemon chiffon")
         self.search_button = Button(text='search', width=3, relief=GROOVE, command=self.start_click_searching)
-        self.search_button.place(x=390, y=215)
+        self.search_button.place(x=390, y=225)
         self.search_button.config(highlightbackground="lemon chiffon")
 
         # 終點
         self.frm_arrive = Frame(width=403, height=40, background="bisque")
-        self.frm_arrive.place(x=50, y=310)
+        self.frm_arrive.place(x=50, y=320)
         self.frm_arrivelist = Frame(width=363, height=40, background="gray98", relief='solid', borderwidth=1)
-        self.frm_arrivelist.place(x=90, y=360)
-        self.frm_arrivebox = Frame(width=30, height=30, background="bisque")
-        self.frm_arrivebox.place(x=55, y=363)
+        self.frm_arrivelist.place(x=90, y=370)
+        self.frm_arrivebox = Frame(width=30, height=40, background="bisque")
+        self.frm_arrivebox.place(x=55, y=373)
         self.lbl_arrive = Label(text = 'Where to?', font = ("Telugu MN", 20), bg="bisque")
-        self.lbl_arrive.place(x=58, y=312)
+        self.lbl_arrive.place(x=58, y=322)
         self.arrive_entry = Entry(bg="gray88", relief=GROOVE, width=18,)#,textvariable=v
-        self.arrive_entry.place(x=215, y=316)
+        self.arrive_entry.place(x=215, y=326)
         self.arrive_entry.config(highlightbackground="bisque")
         self.search_button = Button(text='search', width=3, relief=GROOVE, command=self.arrive_click_searching)
-        self.search_button.place(x=390, y=315)
+        self.search_button.place(x=390, y=325)
         self.search_button.config(highlightbackground="bisque")
 
         # 'Thrifty' button
         self.thrifty_btn = Button(width=10, height=1, text='Thrifty!', relief=GROOVE, font = ("Telugu MN", 20), bg='snow', command=self.thrifty_searching)
-        self.thrifty_btn.place(x=170, y=420)
+        self.thrifty_btn.place(x=170, y=430)
         self.thrifty_btn.config(highlightbackground='white')
 
         # 'Result' board
         self.frm_result = Frame(width=263, height=187, background="white",relief='solid', borderwidth=2)
-        self.frm_result.place(x=473, y=212)
+        self.frm_result.place(x=473, y=222)
         self.frm_result1 = Frame(width=263, height=35, background="white",relief='solid', borderwidth=2)
-        self.frm_result1.place(x=473, y=212)
+        self.frm_result1.place(x=473, y=222)
         self.lbl_result = Label(text = 'Result', font = ("Telugu MN", 15), bg="white")
-        self.lbl_result.place(x=578, y=215)
+        self.lbl_result.place(x=578, y=225)
 
         # 'Reset' button
         self.reset_btn = Button(width=10, height=1, text='Reset', relief=GROOVE, font = ("Telugu MN", 20), bg='snow', command=self.Reset)
-        self.reset_btn.place(x=530, y=420)
+        self.reset_btn.place(x=530, y=430)
         self.reset_btn.config(highlightbackground='white')
 
 
